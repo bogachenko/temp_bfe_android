875d4aba4144ddcfee1d4c2110e12dc9e1b77ec6
Ivan <ivanbogachenco@gmail.com>
2026-08-14 11:28:03 +0300

    feat: add My Files shadow token

--- lib/theme/app_shadows.dart
+++ lib/theme/app_shadows.dart
@@ -10,4 +10,12 @@ abstract final class AppShadows {
       offset: Offset(0, 2),
     ),
   ];
+
+  static const List<BoxShadow> floating = <BoxShadow>[
+    BoxShadow(
+      color: Color(0x24000000),
+      blurRadius: 8,
+      offset: Offset(0, 2),
+    ),
+  ];
 }
