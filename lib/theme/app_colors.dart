0427fc4f7d626c56785b821c25e5939d69df6684
Ivan <ivanbogachenco@gmail.com>
2026-08-14 11:27:15 +0300

    feat: add My Files color tokens

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 
 abstract final class AppColors {
   static const Color neutralBackground1 = Color(0xFFFFFFFF);
+  static const Color neutralBackground2 = Color(0xFFF5F5F5);
   static const Color neutralForeground1 = Color(0xFF242424);
   static const Color neutralForeground2 = Color(0xFF616161);
+  static const Color neutralStroke1 = Color(0xFFD1D1D1);
+  static const Color neutralStroke2 = Color(0xFFE0E0E0);
+
   static const Color brandBackground1 = Color(0xFF006DB8);
   static const Color brandForeground1 = Color(0xFF006DB8);
+  static const Color brandAccent = Color(0xFF0078D4);
+  static const Color brandSelection = Color(0xFFEAF3FB);
   static const Color onBrand = Color(0xFFFFFFFF);
+
+  static const Color folder = Color(0xFFF5B800);
+  static const Color pdf = Color(0xFFD13438);
+  static const Color excel = Color(0xFF107C41);
+  static const Color word = Color(0xFF185ABD);
+  static const Color powerPoint = Color(0xFFD24726);
+  static const Color photo = Color(0xFF0078D4);
+  static const Color textFile = Color(0xFF616161);
+  static const Color video = Color(0xFF8764B8);
+  static const Color genericFile = Color(0xFF8A8886);
+  static const Color destructive = Color(0xFFD13438);
+
   static const Color transparent = Color(0x00000000);
 }
