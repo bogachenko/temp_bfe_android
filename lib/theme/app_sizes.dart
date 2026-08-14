34fcb421ef1039176d0dc46346d96a8202226552
Ivan <ivanbogachenco@gmail.com>
2026-08-14 11:30:07 +0300

    feat: complete My Files geometry tokens

--- lib/theme/app_sizes.dart
+++ lib/theme/app_sizes.dart
@@ -8,6 +8,7 @@ abstract final class AppSizes {
   static const double myFilesModeSwitcherHeight = 36;
   static const double myFilesPremiumButtonSize = 40;
   static const double myFilesPivotBarHeight = 44;
+  static const double myFilesPivotIndicatorHeight = 2;
   static const double myFilesControlsHeight = 40;
 
   static const double myFilesListRowHeight = 60;
@@ -15,6 +16,7 @@ abstract final class AppSizes {
   static const double myFilesItemActionWidth = 48;
   static const double myFilesItemActionIconSize = 24;
   static const double myFilesSharedBadgeSize = 16;
+  static const double myFilesRowDividerStart = 64;
 
   static const double myFilesGridTileMinWidth = 104;
   static const double myFilesGridTileHeight = 132;
@@ -22,14 +24,18 @@ abstract final class AppSizes {
 
   static const double myFilesSearchHeight = 48;
   static const double myFilesSearchIconSize = 20;
+  static const double myFilesSearchFabClearance = 84;
   static const double myFilesFabSize = 56;
   static const double myFilesMiniFabSize = 44;
   static const double myFilesFabIconSize = 28;
+  static const double myFilesFabElevation = 4;
+  static const double myFilesFabMenuBottom = 82;
 
   static const double myFilesPopupWidth = 232;
   static const double myFilesPopupHeaderHeight = 32;
   static const double myFilesPopupItemHeight = 48;
   static const double myFilesPopupIconSize = 20;
+  static const double myFilesPopupElevation = 8;
 
   static const double myFilesBottomSheetHandleWidth = 32;
   static const double myFilesBottomSheetHandleHeight = 4;
