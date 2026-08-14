3a8044c4f2998185af87f25124658f4351b7d4ce
Ivan <ivanbogachenco@gmail.com>
2026-08-14 11:28:12 +0300

    feat: add phone breakpoint for My Files

--- lib/theme/app_breakpoints.dart
+++ lib/theme/app_breakpoints.dart
@@ -1,4 +1,5 @@
 abstract final class AppBreakpoints {
+  static const double phone = 480;
   static const double compact = 600;
   static const double medium = 840;
   static const double expanded = 1200;
