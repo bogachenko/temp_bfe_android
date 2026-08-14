6509ba824cc77964294b92e253b056a878769392
Ivan <ivanbogachenco@gmail.com>
2026-08-14 11:27:54 +0300

    feat: add My Files radius tokens

--- lib/theme/app_radius.dart
+++ lib/theme/app_radius.dart
@@ -2,4 +2,12 @@ abstract final class AppRadius {
   static const double buttonLarge = 8;
   static const double card = 12;
   static const double pill = 24;
+
+  static const double myFilesModeSwitcher = 18;
+  static const double myFilesControl = 20;
+  static const double myFilesGridTile = 8;
+  static const double myFilesPopup = 8;
+  static const double myFilesBottomSheet = 20;
+  static const double myFilesFabLabel = 6;
+  static const double myFilesPivotIndicator = 1;
 }
