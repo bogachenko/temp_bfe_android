93d463e9f5f42d17dc54434c6f0881bfcb8120d1
Ivan <ivanbogachenco@gmail.com>
2026-08-16 20:04:47 +0300

    Fix SignIn illustration Rect construction

--- lib/features/auth/presentation/widgets/sign_in_illustration.dart
+++ lib/features/auth/presentation/widgets/sign_in_illustration.dart
@@ -121,24 +121,24 @@ class _SignInIllustrationPainter extends CustomPainter {
       ..style = PaintingStyle.fill;
 
     canvas.drawOval(
-      const Rect.fromCenter(
-        center: Offset(275.46, 184),
+      Rect.fromCenter(
+        center: const Offset(275.46, 184),
         width: 56,
         height: 36,
       ),
       shadowPaint,
     );
     canvas.drawOval(
-      const Rect.fromCenter(
-        center: Offset(171.15, 281),
+      Rect.fromCenter(
+        center: const Offset(171.15, 281),
         width: 36,
         height: 22,
       ),
       shadowPaint,
     );
     canvas.drawOval(
-      const Rect.fromCenter(
-        center: Offset(416.9, 270),
+      Rect.fromCenter(
+        center: const Offset(416.9, 270),
         width: 36,
         height: 22,
       ),
